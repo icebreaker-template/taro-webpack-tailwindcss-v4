aae0b8423ebbf1a5f7746765f959376564fc48ae
ice breaker <1324318532@qq.com>
2025-03-30 21:01:21 +0800

    chore(deps): upgrade

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,14 +2,18 @@ import { View, Text } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
 import './index.css'
 
-export default function Index () {
+export default function Index() {
   useLoad(() => {
     console.log('Page loaded.')
   })
 
   return (
-    <View className='bg-[#534312] text-[#fff] text-[100rpx]'>
-      <Text>Hello world!</Text>
+    <View className='min-h-screen'>
+      <View className='text-[#16b6c4] text-[54.32rpx]'>欢迎使用 weapp-tailwindcss 模板</View>
+      <View className='bg-[#534312] text-[#fff] text-[100rpx]'>
+        <Text>Hello world!</Text>
+      </View>
     </View>
+
   )
 }
